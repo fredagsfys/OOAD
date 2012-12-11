0061fc0963661dfe37f7e8f479ab34597388a05c
Haris Kljajic <haris.kljajic@hotmail.se>
2012-12-11 14:10:05 +0100

    v1.0

---
+++ Workshop 3/BlackJack_2012/BlackJack/model/rules/DealerWinsIfEqual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class DealerWinsIfEqual : IWinnerRule
+    {
+        public bool IsDealerWinner(Player player, Dealer dealer, int maxScore)
+        {
+            if (player.CalcScore() > maxScore)
+            {
+                return true;
+            }
+            else if (dealer.CalcScore() > maxScore)
+            {
+                return false;
+            }
+            return dealer.CalcScore() >= player.CalcScore();
+        }
+    }
+}
---
+++ Workshop 3/BlackJack_2012/BlackJack/model/rules/Soft17Strategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17Strategy : IHitStrategy
+
+    {
+        private const int g_hitLimit = 17;
+
+        public bool DoHit(model.Player a_dealer)
+        {
+            IEnumerable<Card> cardList = a_dealer.GetHand();
+            if (a_dealer.CalcScore() < g_hitLimit) 
+            {
+                return true;
+            }
+
+            if (a_dealer.CalcScore() == g_hitLimit)
+            {                
+                foreach (Card card in cardList)
+                {
+                    if (card.GetValue() == Card.Value.Ace)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
